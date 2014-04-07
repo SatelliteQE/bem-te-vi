cf8740e3dc59e7b6c2e397a716f3a9b99ace08ee
Elyézer Mendes Rezende <elyezermr@gmail.com>
2014-04-06 23:49:55 -0300

    Add testimony entry admin page options

--- bemtevi/apps/reports/admin.py
+++ bemtevi/apps/reports/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from bemtevi.apps.reports.models import TestimonyEntry
 
-admin.site.register(TestimonyEntry)
+
+class TestimonyEntryModelAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date'
+    list_display = ('path', 'testcases', 'automated_testcases',
+                    'manual_testcases', 'no_docstring_testcases')
+    list_filter = ('date', 'path')
+
+admin.site.register(TestimonyEntry, TestimonyEntryModelAdmin)
